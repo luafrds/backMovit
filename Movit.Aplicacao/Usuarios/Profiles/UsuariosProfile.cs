5cf1acf1a92a72acf0c31d07aa76dc5e587fa65e
Jonathan Costa <nick.costa12@hotmail.com>
2024-01-12 14:11:26 -0300

    MOV-10 - Ajusta profile de usuario.

--- Movit.Aplicacao/Usuarios/Profiles/UsuariosProfile.cs
+++ Movit.Aplicacao/Usuarios/Profiles/UsuariosProfile.cs
@@ -1,9 +1,21 @@
 using AutoMapper;
+using Movit.DataTransfer.Autenticacoes.Response;
+using Movit.DataTransfer.Usuarios.Request;
+using Movit.DataTransfer.Usuarios.Response;
+using Movit.Dominio.Usuarios.Entidades;
+using Movit.Dominio.Usuarios.Repositorios.Filtros;
+using Movit.Dominio.Usuarios.Servicos.Comandos;
 
 namespace Movit.Aplicacao.Usuarios.Profiles
 {
     public class UsuariosProfile : Profile
     {
-        
+        public UsuariosProfile()
+        {
+            CreateMap<Usuario, CadastroResponse>();
+            CreateMap<Usuario, UsuarioResponse>();
+            CreateMap<UsuarioListarRequest, UsuarioListarFiltro>();
+            CreateMap<UsuarioRequest, UsuarioComando>();
+        }
     }
 }
